0aa45db41752df0a68d82cae4c37165e3b1b47a8
Ewan Brain <ewan.brain@gmail.com>
2021-01-06 16:00:44 +0000

    room tests done

--- models/specs/room_spec.js
+++ models/specs/room_spec.js
@@ -2,20 +2,27 @@ const assert = require('assert');
 const Room = require('../room.js');
 
 describe('Room', function () {
-
+    let test_room;
+    beforeEach(function () {
+        test_room = new Room(25)
+    })
 
 
     it('should have an area in sq metres', function () {
-        const room = new Room(25); //Arrange
-        const actual = room.area; // Act
+        // const room = new Room(25); //Arrange
+        const actual = test_room.area; // Act
         assert.strictEqual(actual, 25); //Assert
 
     });
-    xit('should start not painted', function () {
-
+    it('should start not painted', function () {
+        const actual = test_room.painted; // Act
+        assert.strictEqual(actual, false); // Assert
 
     });
-    xit('should be able to be painted', function () {
+    it('should be able to be painted', function () {
+        test_room.paint()
+        const actual = test_room.painted; //Act
+        assert.strictEqual(actual, true); //Assert
 
 
     });
